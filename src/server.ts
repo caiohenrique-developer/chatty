f594c655f69620a4d549289c280bde89ccf47204
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-20 20:27:30 -0300

    :sparkles: [ FEAT ]: Create a new data on .../Settings router

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res
-    .status(200)
-    .json({ message: "É isso ai Caião, vc está mandando muito! ;)" });
-});
+app.use(express.json());
 
-app.post("/users", (req, res) => {
-  return res.status(200).json({ message: "Usuário cadastrado com sucesso!" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port [3333]! 🖥️"));
